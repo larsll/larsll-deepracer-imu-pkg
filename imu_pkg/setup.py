6151ee4195461f1840b7b4d81e8711aa00477372
Lars Lorentz Ludvigsen <larsll@outlook.com>
2021-06-24 15:34:09 -0400

    Renaming launch file

--- imu_pkg/setup.py
+++ imu_pkg/setup.py
@@ -27,7 +27,7 @@ setup(
         ("share/ament_index/resource_index/packages",
             ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name), ["launch/imu_launch.py"])
+        (os.path.join("share", package_name), ["launch/imu_pkg_launch.py"])
     ],
     install_requires=["setuptools"],
     zip_safe=True,
